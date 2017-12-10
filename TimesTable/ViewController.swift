d8d08759af207261552631e0ead8a1d13ceaea6c
Sanuj Bhatia <Sanuj@Sanujs-MacBook-Air.local>
2017-12-10 16:33:31 +0400

    Test101

--- TimesTable/ViewController.swift
+++ TimesTable/ViewController.swift
@@ -8,8 +8,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate  {
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 50
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = table.dequeueReusableCell(withIdentifier: "Cell")
+        
+        cell?.textLabel?.text = String((Int(sliderValue.value * 10)) * (indexPath.row + 1))
+        
+        
+        return cell!
+        
+    }
+    
 
+    @IBAction func sliderChanged(_ sender: UISlider) {
+        
+        table.reloadData()
+        
+    }
+    
+    @IBOutlet weak var sliderValue: UISlider!
+    
+    
+    @IBOutlet weak var table: UITableView!
+    
+    
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
